07788ee4ed4c638ec8739e2b6af9a6458b8f81a8
fhkhoury <f.khouryat@gmail.com>
2016-08-09 16:00:51 +0200

    Creation of the project

---
+++ SwiftSampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSampleApp
+//
+//  Created by François K on 09/08/2016.
+//  Copyright © 2016 François K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
