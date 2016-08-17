d67ebfbd078b91270b5948887f44f95459df9002
Julien Gil <jgil@student.42.fr>
2016-08-17 14:16:48 +0200

    creation of an empty handler class + reorganization of the project folder architecture

---
+++ Cargo/Handlers/GoogleAnalytics/CARGoogleAnalyticsTagHandler.swift
@@ -0,0 +1,14 @@
+//
+//  CARGoogleAnalyticsTagHandler.swift
+//  Cargo
+//
+//  Created by Med on 17/08/16.
+//  Copyright © 2016 François K. All rights reserved.
+//
+
+import Foundation
+
+class CARGoogleAnalyticsTagHandler: CARTagHandler {
+    
+    
+}
