db49a64defca3ac6d6aa478d7e32d0a41d9c46b3
Julien Gil <jgil@student.42.fr>
2016-08-17 16:14:16 +0200

    comments in CARTagHandler

--- Cargo/Handlers/CARTagHandler.swift
+++ Cargo/Handlers/CARTagHandler.swift
@@ -9,7 +9,9 @@
 import Foundation
 
 class CARTagHandler : NSObject, TAGFunctionCallTagHandler {
-    
+
+/* ********************************* Variables Declaration ********************************* */
+
     var key: String;
     var name: String;
     var initialized: Bool = false ;
@@ -17,20 +19,45 @@ class CARTagHandler : NSObject, TAGFunctionCallTagHandler {
 
     let cargo = Cargo.sharedHelper;
 
+/* ************************************* Initializer *************************************** */
+
+    /**
+     *  Initialize the variables for the handler
+     *
+     *  @param key  The key for the handler
+     *  @param name The name of the handler
+     */
     init(key:String, name:String){
         self.key = key;
         self.name = name;
     }
 
-    func execute(tagName:String, parameters:[NSObject : AnyObject]!){
+/* ********************************* Methods declaration *********************************** */
+
+    /**
+     *  Called when an execute child method is called. Logs the call
+     *
+     *  @param tagName  The tag name
+     *  @param parameters   Dictionary of parameters
+     */
+    func execute(tagName:String, parameters:[NSObject : AnyObject]){
         print("Function \(tagName) has been received with parameters \(parameters) ");
     }
 
+    /**
+     *  Logs when a tag doesn't match a method
+     *
+     *  @param handler  The handler it happens in
+     *  @param tagName  The tag name which doesn't match
+     */
     func noTagMatch(handler: CARTagHandler, tagName: String) {
-        let infoMessage = "\(tagName) does not match any known tag in handler \(handler.name)";
-        cargo.logger.carLog(kTAGLoggerLogLevelInfo, message: infoMessage);
+        let infoMessage = "\(tagName) does not match any known tag";
+        cargo.logger.carLog(kTAGLoggerLogLevelInfo, handler: handler, message: infoMessage);
     }
 
+    /**
+     *  Called when the handler is valid
+     */
     func validate(){
         valid = true;
     }
