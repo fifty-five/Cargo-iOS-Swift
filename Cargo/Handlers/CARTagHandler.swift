fe9ccb8e9b44474de762bf30cb0e1ec5b2525b55
Julien Gil <jgil@student.42.fr>
2016-08-17 15:18:33 +0200

    added a reference to a cargo instance in each handler + a log function for a unknown tagName

--- Cargo/Handlers/CARTagHandler.swift
+++ Cargo/Handlers/CARTagHandler.swift
@@ -14,18 +14,25 @@ class CARTagHandler : NSObject, TAGFunctionCallTagHandler {
     var name: String;
     var initialized: Bool = false ;
     var valid: Bool = false;
-    
+
+    let cargo = Cargo.sharedHelper;
+
     init(key:String, name:String){
         self.key = key;
         self.name = name;
     }
-    
+
     func execute(tagName:String, parameters:[NSObject : AnyObject]!){
         print("Function \(tagName) has been received with parameters \(parameters) ");
     }
-    
+
+    func noTagMatch(handler: CARTagHandler, tagName: String) {
+        let infoMessage = "\(tagName) does not match any known tag in handler \(handler.name)";
+        cargo.logger.carLog(kTAGLoggerLogLevelInfo, message: infoMessage);
+    }
+
     func validate(){
         valid = true;
     }
-    
+
 }
