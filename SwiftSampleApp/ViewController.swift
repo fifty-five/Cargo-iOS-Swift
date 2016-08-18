2066858af9139975d4e8c7aa031d6ad29c9914f7
Julien Gil <jgil@student.42.fr>
2016-08-18 17:07:33 +0200

    small interface for tests

--- SwiftSampleApp/ViewController.swift
+++ SwiftSampleApp/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +20,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func pressedEvent(sender : AnyObject) {
+        let dataLayer = Cargo.sharedHelper.tagManager.dataLayer;
+        dataLayer.push(["event": "testGA"]);
+    }
+    
+    @IBAction func pressedScreen(sender : AnyObject) {
+        let dataLayer = Cargo.sharedHelper.tagManager.dataLayer;
+        dataLayer.push(["event": "openScreen", SCREEN_NAME: "home_screen"])
+    }
 
 }
 
