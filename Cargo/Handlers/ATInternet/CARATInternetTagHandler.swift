f028c4e940b192772e0b9b48bd478a1e01e46654
Julien Gil <jgil@student.42.fr>
2016-09-26 16:13:42 +0200

    adding AT Internet to cargo

---
+++ Cargo/Handlers/ATInternet/CARATInternetTagHandler.swift
@@ -0,0 +1,173 @@
+//
+//  CARATInternetTagHandler.swift
+//  Cargo
+//
+//  Created by Julien Gil on 06/09/16.
+//  Copyright © 2016 fifty-five All rights reserved.
+//
+
+import Foundation
+import Tracker
+
+class CARATInternetTagHandler: CARTagHandler {
+
+/* ********************************* Variables Declaration ********************************* */
+
+    let AT_init = "AT_init";
+    let AT_setConfig = "AT_setConfig";
+    let AT_tagScreen = "AT_tagScreen";
+
+    let TRACKER_NAME = "trackerName";
+    let OVERRIDE = "override";
+    let CHAPTER1 = "chapter1";
+    let CHAPTER2 = "chapter2";
+    let CHAPTER3 = "chapter3";
+    let BASKET = "isBasketView";
+    let ACTION = "action";
+
+    let tracker: Tracker;
+
+/* ************************************* Initializer *************************************** */
+
+    /**
+     *  Initialize the handler
+     */
+    init() {
+        super.init(key: "AT", name: "AT Internet");
+
+        cargo.registerTagHandler(self, key: AT_init);
+        cargo.registerTagHandler(self, key: AT_setConfig);
+        cargo.registerTagHandler(self, key: AT_tagScreen);
+    }
+
+/* ******************************** Core handler methods *********************************** */
+
+    /**
+     *  Call back from GTM container to execute a specific action
+     *  after tag and parameters are received
+     *
+     *  @param tagName  The tag name
+     *  @param parameters   Dictionary of parameters
+     */
+    override func execute(tagName: String, parameters: [NSObject : AnyObject]) {
+        super.execute(tagName, parameters: parameters);
+
+        switch (tagName) {
+        case AT_init:
+            self.initialize(parameters);
+            break ;
+        case AT_setConfig:
+            self.setConfig(parameters);
+            break ;
+        case AT_tagScreen:
+            self.tagScreen(parameters);
+            break ;
+        default:
+            noTagMatch(self, tagName: tagName);
+        }
+    }
+
+    /**
+     * The method you have to call first, because it initializes
+     * the AT Internet tracker with the parameters you give.
+     *
+     *  @param parameters   Dictionary of parameters
+     */
+    func initialize(parameters: [NSObject : AnyObject]) {
+        var params = parameters;
+
+        // we check if the required name is set, and then initialize the tracker with required values
+        if let trackerName = params[TRACKER_NAME] {
+            params.removeValueForKey(TRACKER_NAME);
+            tracker = ATInternet.sharedInstance.tracker(trackerName, configuration:params);
+            cargo.logger.logParamSetWithSuccess(TRACKER_NAME, value: parameters);
+            self.initialized = true;
+        }
+        else {
+            cargo.logger.logMissingParam(TRACKER_NAME, methodName: AT_init, handler: self);
+        }
+    }
+
+    /**
+     * The method you may call if you want to reconfigure your tracker
+     *
+     *  @param parameters   Dictionary of parameters
+     */
+    func setConfig(parameters: [NSObject : AnyObject]) {
+        var params = parameters;
+
+        if let override = params[OVERRIDE] {
+            params.removeValueForKey(OVERRIDE);
+            tracker.setConfig(params, override: override) { (isSet) -> Void in
+                cargo.logger.carLog(kTAGLoggerLogLevelInfo, handler: self,
+                                    message: "tracker reconfigured with \(params) and override set to \(override)");
+            }
+        }
+    }
+
+/* ********************************** Specific methods ************************************* */
+
+    func tagScreen(parameters: [NSObject: AnyObject]) {
+
+        if (initialized) {
+            if let screenName = parameters[SCREEN_NAME] {
+                var screen = tracker.screens.add(screenName as! String);
+                cargo.logger.logParamSetWithSuccess(SCREEN_NAME, value: screen.name);
+
+                screen = self.addChapters(parameters, screen: screen);
+                screen = self.setAdditionalProperties(parameters, screen: screen);
+
+                screen.sendView();
+            }
+            else {
+                cargo.logger.logMissingParam(SCREEN_NAME, methodName: tagScreen, handler: self);
+            }
+        }
+        else {
+            cargo.logger.logUninitializedFramework(self);
+        }
+    }
+
+/* *********************************** Utility methods ************************************* */
+
+    private func addChapters(parameters: [NSObject: AnyObject], screen: screen) {
+        var screen = screen;
+
+        if let chapter1 = parameters[CHAPTER1] {
+            screen.chapter1 = chapter1 as! String;
+            cargo.logger.logParamSetWithSuccess(CHAPTER1, value: screen.chapter1);
+
+            if let chapter2 = parameters[CHAPTER2] {
+                screen.chapter2 = chapter2 as! String;
+                cargo.logger.logParamSetWithSuccess(CHAPTER2, value: screen.chapter2);
+
+                if let chapter3 = parameters[CHAPTER3] {
+                    screen.chapter2 = chapter3 as! String;
+                    cargo.logger.logParamSetWithSuccess(CHAPTER3, value: screen.chapter3);
+                }
+            }
+        }
+        return screen;
+    }
+
+    private func setAdditionalProperties(parameters: [NSObject: AnyObject], screen: screen) {
+        var screen = screen;
+
+        if let level2 = parameters[LEVEL2] {
+            screen.level2 = level2 as! Int;
+            cargo.logger.logParamSetWithSuccess(SCREEN_NAME, value: screen.name);
+        }
+        if let basket = parameters[BASKET] {
+            screen.isBasketScreen = basket as! Bool;
+            cargo.logger.logParamSetWithSuccess(BASKET, value: screen.isBasketView);
+        }
+        if let action = parameters[ACTION] {
+            screen.action = action as! Action.View;
+            cargo.logger.logParamSetWithSuccess(ACTION, value: screen.action);
+        }
+
+        return screen;
+    }
+
+}
+
