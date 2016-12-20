e8b7516853571851e83e11c3d8bb16d724f260ed
juliengil <julien.gil@fifty-five.com>
2016-12-20 12:12:36 +0100

    changed the look of the app in order to add more tests

--- SwiftSampleApp/ViewController.swift
+++ SwiftSampleApp/ViewController.swift
@@ -24,25 +24,25 @@ class ViewController: UIViewController {
     @IBAction func pressedEvent(_ sender : AnyObject) {
         let dataLayer = Cargo.sharedHelper.tagManager.dataLayer;
 
-        dataLayer?.push(["event": "testGA"]);
-        dataLayer?.push(["event": "testFirebase"]);
-        dataLayer?.push(["event": "testTune", "eventName": "a cool event", "eventCurrencyCode": "USD"]);
-        dataLayer?.push(["event": "testFacebook", "eventName": "a super cool event", "valueToSum": 123]);
-        dataLayer?.push(["event": "testAT"]);
+        dataLayer?.push(["event": "tagEvent"]);
     }
 
     @IBAction func pressedUser(_ sender : AnyObject) {
         let dataLayer = Cargo.sharedHelper.tagManager.dataLayer;
 
-        dataLayer?.push(["event": "setUser"]);
-        dataLayer?.push(["event": "initialize"]);
+        dataLayer?.push(["event": "identify"]);
     }
 
     @IBAction func pressedScreen(_ sender : AnyObject) {
         let dataLayer = Cargo.sharedHelper.tagManager.dataLayer;
 
-        dataLayer?.push(["event": "openScreen", SCREEN_NAME: "home_screen"]);
-        dataLayer?.push(["event": "purchase", "purchaseAmount": 60, "currencyCode": "USD"]); //pour tester l'event purchase du handler Facebook
+        dataLayer?.push(["event": "tagScreen"]);
+    }
+
+    @IBAction func pressedPurchase(_ sender : AnyObject) {
+        let dataLayer = Cargo.sharedHelper.tagManager.dataLayer;
+        
+        dataLayer?.push(["event": "tagPurchase"]);
     }
 
 }
