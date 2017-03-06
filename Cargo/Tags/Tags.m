7b78529e832a0ef9eafecb0b8e0e4905ad0b12fb
Julien Gil <julien.gil@fifty-five.com>
2017-03-06 15:33:34 +0100

    update the swift tag class to an objC class

---
+++ Cargo/Tags/Tags.m
@@ -0,0 +1,19 @@
+//
+//  Tags.m
+//  Cargo
+//
+//  Created by Julien Gil on 06/03/2017.
+//  Copyright © 2017 François K. All rights reserved.
+//
+
+#import "Tags.h"
+#import "Cargo-Swift.h"
+
+@implementation Tags
+
+- (NSObject*)executeWithParameters:(NSDictionary*)parameters {
+    [[Cargo getInstance] execute:parameters];
+    return nil;
+}
+
+@end
