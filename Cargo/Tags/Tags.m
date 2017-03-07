17307012ee2e03228f1a2f7a2fe7524cf4b71ed5
Julien Gil <julien.gil@fifty-five.com>
2017-03-07 18:21:02 +0100

    update of the Tags.m docuementation

--- Cargo/Tags/Tags.m
+++ Cargo/Tags/Tags.m
@@ -11,8 +11,17 @@
 
 @implementation Tags
 
+/**
+ Function Call tags let you execute pre-registered functions (e.g. to trigger hits for additional 
+ measurement tools that are not currently supported with tag templates in Google Tag Manager).
+
+ @param parameters The parameters of the event received
+ @return nil
+ */
 - (NSObject*)executeWithParameters:(NSDictionary*)parameters {
+    // redirects the call to the Cargo Class which handles it.
     [[Cargo getInstance] execute:parameters];
+    // notifies the CargoItem class that a tag has been fired.
     [CargoItem notifyTagFired];
     return nil;
 }
