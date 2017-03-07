8cf976cdcaa818f2ee97a2907e94dd547bc2372f
Julien Gil <julien.gil@fifty-five.com>
2017-03-07 15:06:20 +0100

    update of the CargoItem class as in objC and android version, compliant with GTM v5

--- Cargo/CargoItem.swift
+++ Cargo/CargoItem.swift
@@ -10,10 +10,10 @@ import Foundation
 
 
 /// Create an Item object with this class in order to send item objects through Cargo
-class CargoItem : NSObject {
+@objc class CargoItem : NSObject {
     
     /// name of the item
-    var item: String!;
+    var name: String!;
     /// unit price of the item
     var unitPrice: CGFloat;
     /// number of items concerned
@@ -31,22 +31,61 @@ class CargoItem : NSObject {
     /// attribute of the item
     var attribute5: String!;
 
+    static var itemsArray : [CargoItem]?;
+    static var tagFiredSinceLastChange = false;
+
+
+    class func attachItemToEvent(item: CargoItem) {
+        self.emptyListIfTagHasBeenFired();
+        if ((self.itemsArray?.append(item)) == nil) {
+            self.itemsArray = [item];
+        }
+    }
+
+    class func emptyListIfTagHasBeenFired() {
+        if (self.tagFiredSinceLastChange) {
+            self.tagFiredSinceLastChange = false;
+            self.itemsArray = nil;
+        }
+    }
+
+    /// A getter for the NSMutableArray of items which will be sent to the next "item relative" event.
+    /// May be used to modify some objects before setting a new Array with the 'setItemsArray' method.
+    ///
+    /// @return an NSMutableArray of CargoItem objects.
+    class func getItemsArray() -> ([CargoItem]) {
+        return self.itemsArray!;
+    }
     
+
+    /// Sets the array of items which will be sent to the next "item relative" event with a new value.
+    ///
+    /// @param newItemsArray A new array of CargoItem objects, which value can be null.
+    class func setNewItemsArray(newItemsArray: [CargoItem]) {
+        self.emptyListIfTagHasBeenFired();
+        self.itemsArray = newItemsArray;
+    }
+
+    /// A method called whenever a tag is received in the Tags class.
+    @objc class func notifyTagFired() {
+        self.tagFiredSinceLastChange = true;
+    }
+
     /// Creates a CargoItem, which will be changed into an Item object for a specific SDK
-    /// The revenue is automatically calculated with (quantity * unit price)
+    /// The revenue will be automatically calculated with (quantity * unit price) within Tune SDK
     ///
     /// - Parameters:
     ///   - name: name of the item
     ///   - unitPrice: unit price of the item
     ///   - quantity: amount of all these items
     public init(name: String!, unitPrice: CGFloat, quantity: UInt) {
-        self.item = name;
+        self.name = name;
         self.unitPrice = unitPrice;
         self.quantity = quantity;
         self.revenue = unitPrice * CGFloat(quantity);
     }
 
-    
+
     /// Creates a CargoItem, which will be changed into an Item object for a specific SDK
     ///
     /// - Parameters:
@@ -55,16 +94,16 @@ class CargoItem : NSObject {
     ///   - quantity: amount of all these items
     ///   - revenue: total cost of all these items
     public init(name: String!, unitPrice: CGFloat, quantity: UInt, revenue: CGFloat) {
-        self.item = name;
+        self.name = name;
         self.unitPrice = unitPrice;
         self.quantity = quantity;
         self.revenue = revenue;
     }
 
-    
+
     /// A String representation for the current object.
     override var description: String {
-        var json: String = "{\n\"id\": \"\(self.item!)\",\n \"unitPrice\": \"\(self.unitPrice)\",\n" +
+        var json: String = "{\n\"name\": \"\(self.name!)\",\n \"unitPrice\": \"\(self.unitPrice)\",\n" +
         "\"quantity\": \"\(self.quantity)\",\n\"revenue\": \"\(self.revenue)\"";
         if ((attribute1) != nil) {
             json = json + ",\n\"attribute1\": \"\(self.attribute1!)\"";
@@ -85,48 +124,4 @@ class CargoItem : NSObject {
         return json;
     }
 
-    
-    /// This class method has to be used before sending a CargoItem array to the DataLayer
-    /// The array given as parameter is turned into a flat json String.
-    ///
-    /// - Parameter itemArray: the array of CargoItem attached to this event
-    /// - Returns: a flat json, as a String, which will be turned back into an array by the handler.
-    class func toGTM(itemArray: [CargoItem]) -> (String) {
-        var tempDictArray: [Dictionary<String, AnyHashable>] = [];
-        var string: String!;
-
-        // turns the CargoItem objects as dictionaries
-        for item in itemArray {
-            var dic : Dictionary<String, AnyHashable> = [ "name" : item.item!, "unitPrice" : item.unitPrice,
-                "quantity" : item.quantity, "revenue" : item.revenue];
-            if ((item.attribute1) != nil) {
-                dic["attribute1"] = item.attribute1!;
-            }
-            if ((item.attribute2) != nil) {
-                dic["attribute2"] = item.attribute2!;
-            }
-            if ((item.attribute3) != nil) {
-                dic["attribute3"] = item.attribute3!;
-            }
-            if ((item.attribute4) != nil) {
-                dic["attribute4"] = item.attribute4!;
-            }
-            if ((item.attribute5) != nil) {
-                dic["attribute5"] = item.attribute5!;
-            }
-            // add the dictionary in a array
-            tempDictArray.append(dic);
-        }
-        do {
-            // array encoded in JSON data
-            let jsonData = try JSONSerialization.data(withJSONObject: tempDictArray);
-            // JSON data converted to String
-            string = String(data: jsonData, encoding: String.Encoding.utf8);
-        } catch {
-            print(error.localizedDescription);
-            Cargo.getInstance().logger.carLog(.error, message: "Unable to convert" +
-                " [CargoItem] to Data type in class method CargoItem.toGTM()");
-        }
-        return string!;
-    }
 }
