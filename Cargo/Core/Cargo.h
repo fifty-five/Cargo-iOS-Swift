e5fea04e468ede88f8aefdbab305dd4be748d75e
Julien Gil <julien.gil@fifty-five.com>
2017-03-09 15:12:21 +0100

    adding umbrella header

---
+++ Cargo/Core/Cargo.h
@@ -0,0 +1,13 @@
+//
+//  Cargo.h
+//  Cargo
+//
+//  Created by Julien Gil on 09/03/2017.
+//  Copyright © 2017 François K. All rights reserved.
+//
+
+#ifndef Cargo_h
+#define Cargo_h
+
+
+#endif /* Cargo_h */
