2c239b3e5f66a3945940cb41221391c78503f714
Julien Gil <julien.gil@fifty-five.com>
2017-08-03 18:51:29 +0200

    tests

--- CargoTests/CargoTests.swift
+++ CargoTests/CargoTests.swift
@@ -23,7 +23,7 @@ class CargoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XTCAssert(true);
+        XCTAssert(true);
     }
     
     func testPerformanceExample() {
